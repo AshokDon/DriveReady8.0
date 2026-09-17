ccc2cdfe7825f3ffc91692d6f45b5af22f76eac5
DIVYATEJA785 <24B11CS115@adityauniversity.in>
2026-09-17 14:33:37 +0530

    Implement Singleton pattern in client_task1.py

--- python_Tasks/SingleTone_Task-1/client_task1.py
+++ python_Tasks/SingleTone_Task-1/client_task1.py
@@ -5,56 +5,58 @@ def main():
 
     # TODO 1:
     # Get the first Configuration Manager instance.
-    config1 = None
+    config1 = FileBasedConfigurationManager.get_instance()
 
     # TODO 2:
-    # Store configuration values.
+    # Store configuration values
     #
-    # config1.set_configuration("app.name", "MyApplication")
-    # config1.set_configuration("max.connections", 100)
-    # config1.set_configuration("timeout", 30.5)
+     config1.set_configuration("app.name", "MyApplication")
+     config1.set_configuration("max.connections", 100)
+     config1.set_configuration("timeout", 30.5)
 
     # TODO 3:
     # Read the configuration values.
     #
-    # print("App Name:", ...)
-    # print("Max Connections:", ...)
-    # print("Timeout:", ...)
+     print("App Name:", config.get_configuration("app.name"))
+     print("Max Connections:", config.get_configuration("max.connections"))
+     print("Timeout:",config.get_configuration("timeout"))
 
     # TODO 4:
     # Get the Singleton instance again.
-    config2 = None
+    config2 = FileBasedConfigurationManager.get_instance()
 
     # TODO 5:
     # Verify that config1 and config2 are the same object.
     #
-    # print("Same instance:", ...)
+     print("Same instance:", config1 is config2)
 
     # TODO 6:
     # Remove the timeout configuration.
     #
     # config1.remove_configuration("timeout")
-    # print("Timeout after removal:", ...)
+     print("Timeout after removal:",config.get_configuration("timeout"))
 
     # TODO 7:
     # Clear all configurations.
     #
     # config1.clear()
-    # print("App Name after clear:", ...)
+     print("App Name after clear:", config.get_configuration("app.name"))
 
     # TODO 8:
     # Reset the Singleton.
     #
-    # FileBasedConfigurationManager.reset_instance()
+     config1 = FileBasedConfigurationManager.get_instance()
+     FileBasedConfigurationManager.reset_instance()
+     config2 = FileBasedConfigurationManager.get_instance()
 
     # TODO 9:
     # Get the Configuration Manager again.
-    config3 = None
+    config3 =  FileBasedConfigurationManager.get_instance()
 
     # TODO 10:
     # Verify that config1 and config3 are different objects.
     #
-    # print("New instance after reset:", ...)
+     print("New instance after reset:",config1 is config2 )
 
 
 if __name__ == "__main__":
